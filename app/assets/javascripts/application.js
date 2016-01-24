bed3ef154f95fadf16b3926fe16d2c9f4b836700
iToshk <iwasaki@bbank.jp>
2016-01-24 23:10:49 +0900

    Able to read application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,45 +13,51 @@
 //= require jquery
 //= require jquery-ui
 //= require angular
+//= require angular-animate
+//= require angular-bootstrap
+//= require angular-ui-router
+//= require angular-animate
+//= require underscore
 //= require bootstrap
 //= require bootstrap-sass
-//= require underscore
 //= require_tree .
 
-$scope.scrollSmoothTo = function(elementOrName){
-  var element = {'true':$('*[name='+elementOrName+'],'+
-                          '#'+elementOrName).eq(0),
-                 'false':elementOrName}[typeof elementOrName === 'string'];
-  if(!element){
-    return;
-  }
-  if(!element.offset){
-    element = $(element);
-  }
-  if(!element.offset){
-    return;
-  }
-  var top = Math.max(0, element.offset().top - ($(window).height()/8));
-  $('html,body').
-  animate(
-    {'scrollTop': top},
-    'slow'
-  );
-};
 
-(function() {
-    var app = angular.module("app", [
-        "ngAnimate",
-        "ui.bootstrap",
-    ]);
 
-    angular.module("app").controller("ApplicationController", function(
-        $scope)
-        {
-          $scope.mens_floor = "Men's Floor";
+
+var app = angular.module("app", [
+    "ngAnimate",
+    "ui.router",
+    "ui.bootstrap"
+]);
+
+app.controller("ApplicationController", ['$scope', function(
+    $scope)
+{
+    $scope.mens_floor = "床（男子）";
+
+    $scope.scrollSmoothTo = function(elementOrName){
+        var element = {'true':$('*[name='+elementOrName+'],'+
+                                '#'+elementOrName).eq(0),
+                       'false':elementOrName}[typeof elementOrName === 'string'];
+        if(!element){
+            return;
         }
-    )
-})
+        if(!element.offset){
+            element = $(element);
+        }
+        if(!element.offset){
+            return;
+        }
+        var top = Math.max(0, element.offset().top - ($(window).height()/8));
+        $('html,body').
+        animate(
+            {'scrollTop': top},
+            'slow'
+        );
+    };
+}]);
+
 
 
 
